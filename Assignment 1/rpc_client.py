b7094bca0c9e5a32143515926ae19cb6b6e0d379
Akileswaran-7 <akileswaransaravanan2@gmail.com>
2026-01-31 19:02:46 +0530

    Fixed folder structure: Assignment 1 and Assignment 2

--- Assignment 1/rpc_client.py
+++ Assignment 1/rpc_client.py
@@ -11,6 +11,6 @@ import xmlrpc.client
 # Replace with your EC2 public IP
 server = xmlrpc.client.ServerProxy("http://EC2_PUBLIC_IP:8000")
 
-print(server.add_student("CS101", "Arjun", 82))
+print(server.add_student("CS101", "Akil", 82))
 print("Student Details:", server.get_student("CS101"))
 print("Grade:", server.calculate_grade("CS101"))
